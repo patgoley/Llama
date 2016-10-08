efc2a95b6a65f46877832df404bc7c39435d6909
Patrick Goley <patrick.goley@gmail.com>
2016-10-08 15:02:01 -0500

    Initial Commit

---
+++ Llama/Llama.h
@@ -0,0 +1,19 @@
+//
+//  Llama.h
+//  Llama
+//
+//  Created by Patrick Goley on 10/8/16.
+//  Copyright © 2016 patrickgoley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Llama.
+FOUNDATION_EXPORT double LlamaVersionNumber;
+
+//! Project version string for Llama.
+FOUNDATION_EXPORT const unsigned char LlamaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Llama/PublicHeader.h>
+
+
